d3a8684889e58b0aa93f1ff689d61627c098d1a0
Julio Labarca <jlabarca25@gmail.com>
2022-07-22 12:02:32 -0400

    ECS manual start

---
+++ Assets/Scripts/ECS/EcsStartup.cs
@@ -0,0 +1,36 @@
+﻿using ECSFlowField;
+using Unity.Entities;
+using Unity.Transforms;
+
+public static class GameInitializer
+{
+    public static void InitializeSystemWorkflow()
+    {
+        InitializeSystems();
+    }
+
+    private static void InitializeSystems()
+    {
+        var world = World.DefaultGameObjectInjectionWorld;
+
+        //System Group Handles (From Unity)
+        var initialization = world.GetOrCreateSystem<InitializationSystemGroup>();
+        var simulation = world.GetOrCreateSystem<SimulationSystemGroup>();
+        var transform = world.GetOrCreateSystem<TransformSystemGroup>();
+        var lateSimulation = world.GetOrCreateSystem<LateSimulationSystemGroup>();
+        var presentation = world.GetOrCreateSystem<PresentationSystemGroup>();
+
+        var flowFieldSystemGroup = world.GetOrCreateSystem<FlowFieldSystemGroup>();
+
+        //Adding Managers as systems
+        initialization.AddSystemToUpdateList(flowFieldSystemGroup);
+
+
+        //Sorting
+        initialization.SortSystems();
+        simulation.SortSystems();
+        transform.SortSystems();
+        lateSimulation.SortSystems();
+        presentation.SortSystems();
+    }
+}
